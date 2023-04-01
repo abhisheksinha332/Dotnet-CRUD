075583686ac59ffbc3ae7715afd29665087c1ece
Abhishek Sinha (Bosch Global Software Technolo) <v-abhissinha@microsoft.com>
2023-04-01 23:40:35 +0530

    Front end setup

--- Reactivities/API/Program.cs
+++ Reactivities/API/Program.cs
@@ -12,6 +12,11 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DataContext>(opt =>{
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors(opt =>{
+    opt.AddPolicy("CorsPolicy", policy =>{
+        policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http:localhost:3000");
+    })
+})
 
 var app = builder.Build();
 
